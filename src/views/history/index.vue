fb2d019cabef5bd2fa9acabb8b4a85da9db4e7ab
张燕龙 <yanlongzhang1990@gmail.com>
2024-08-01 09:11:35 +0800

    feat(*): 历史报告列表搜索优化

--- src/views/history/index.vue
+++ src/views/history/index.vue
@@ -17,7 +17,7 @@
         />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSearch">查询</el-button>
+        <el-button type="primary" @click="() => onSearch()">查询</el-button>
         <el-button @click="onReset">重置</el-button>
       </el-form-item>
     </el-form>
